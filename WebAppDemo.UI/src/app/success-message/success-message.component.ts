9ef62d2810d49c2f8ca5d20b452cdf9234386714
Robin Smit <nyxero@web.de>
2023-06-23 13:41:03 +0200

    + Complete success-message component

--- WebAppDemo.UI/src/app/success-message/success-message.component.ts
+++ WebAppDemo.UI/src/app/success-message/success-message.component.ts
@@ -1,24 +1,39 @@
-import { Component, Input } from '@angular/core';
-import { Message } from './message.model';
-import { MessageType } from '../shared/messageType.enum';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { MatIconRegistry } from '@angular/material/icon';
+import { IconHelper } from '../shared/iconHelper';
+import { IconType } from '../shared/iconType.enum';
 
 @Component({
   selector: 'app-success-message',
   templateUrl: './success-message.component.html',
   styleUrls: ['./success-message.component.scss']
 })
-export class SuccessMessageComponent {
-  @Input() visible: boolean = false;
+export class SuccessMessageComponent implements OnInit {
+  @ViewChild('messageBox') messageBox!: ElementRef; 
+  @Input() isVisible: boolean = true;
+  @Input() visibilityTime: number = 0;
   @Input() title: string = "Success";
   @Input() description: string = "";
+  iconType = IconType;
+
+  get getVisibility(): string { return this.isVisible ? "visibility: visiable" : "visibility: hidden" }
 
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
-    iconRegistry.addSvgIcon('close', sanitizer.bypassSecurityTrustResourceUrl('assets/svg/close.svg'))
+    IconHelper.registerIcons([IconType.Close], iconRegistry, sanitizer);
   }
 
-  onClose(): void {
-    this.visible = false
+  ngOnInit(): void {
+    this.setupVisibilityTime();
+  }
+
+  setupVisibilityTime(): void {
+    if(this.visibilityTime > 0) {
+      setTimeout(() => { this.setVisibility(false) }, this.visibilityTime);
+    }
+  }
+
+  setVisibility(visibility: boolean): void {
+    this.isVisible = visibility;
   }
 }
